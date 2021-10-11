6772ae616061ebb9e4e6f7a60092a1d40e965a5e
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2021-10-11 12:47:01 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'tsx')
+app.use(express.static('public'))
+app.engine('tsx', require('express-react-views').createEngine());
+
+app.get('/call', (req, res) => {
+  res.redirect(`/call/${uuidV4()}`)
+})
+
+app.get('/call/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(process.env.PORT || 3000)
