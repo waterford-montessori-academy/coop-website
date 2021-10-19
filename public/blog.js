d4d5a026e6561d88c6a7eb05956316ce9ac748f5
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2021-10-19 12:51:16 -0400

    Update blog.js

--- public/blog.js
+++ public/blog.js
@@ -1,5 +1,5 @@
 // @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache-2.0
 $.getJSON("https://raw.githubusercontent.com/waterford-montessori-academy/coop-blog/main/posts/2021/october/19.json", function (data) {
-  alert(data.description)
+  document.write("<h1>" + data.title + "</h1><br/>" + data.content);
 })
   
