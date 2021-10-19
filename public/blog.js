3a3a7596787a80b0042500038352b0c53da4e025
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2021-10-19 13:42:13 -0400

    Update blog.js

--- public/blog.js
+++ public/blog.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
   const day = new Date().getDate();
   const year = new Date().getFullYear();
   $.getJSON("https://raw.githubusercontent.com/waterford-montessori-academy/coop-blog/main/posts/" + year + "/" + month + "/" + day + ".json", function (data) {
-    $(document.body).append("<h1>" + data.title + "</h1>" + "<h2>Today's blog post, courtesy of " + data.author + "</h2><br />" + data.content);
+    $(document.body).append("<h1>" + data.title + "</h1>" + "<h3>Today's blog post, courtesy of " + data.author + "</h3><br /><h5>" + data.content + "</h5>");
   })
 })
 // @license-end  
