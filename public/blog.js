d57edafbf598512d1ff270607b12671e488cc202
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2021-10-21 14:10:09 -0400

    Update blog.js

--- public/blog.js
+++ public/blog.js
@@ -3,10 +3,10 @@ $(document).ready(() => {
   const month = new Date().toLocaleString("default", { month: "long" }).toLowerCase();
   const day = new Date().getDate();
   const year = new Date().getFullYear();
-  const url = "https://raw.githubusercontent.com/waterford-montessori-academy/coop-blog/main/posts/" + year + "/" + month + "/" + day + ".json"
-  $.getJSON(, function (data) {
+  const url = "https://raw.githubusercontent.com/waterford-montessori-academy/coop-blog/main/posts/" + year + "/" + month + "/" + day + ".json";
+  $.getJSON(url, function (data) {
     const http = new XMLHttpRequest();
-    http.open('HEAD', "https://raw.githubusercontent.com/waterford-montessori-academy/coop-blog/main/posts/" + year + "/" + month + "/" + day + ".json", false);
+    http.open('HEAD', url, false);
     http.send();
     if (http.status != 404) {
       $(document.body).append("<img style='display: block; margin-left: auto; margin-right: auto; width: " + data.width + "; height: " + data.height +  ";' src='" + data.picture + "'><h1>" + data.title + "</h1>" + "<h3>Today's blog post, courtesy of " + data.author + "</h3><h4>" + data.description + "</h4><h5>" + data.content + "</h5>");
