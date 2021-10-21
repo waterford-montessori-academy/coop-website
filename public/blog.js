a485c281de5d3e7b64617a5c2f01fc612d2a8b7f
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2021-10-21 13:17:14 -0400

    Update blog.js

--- public/blog.js
+++ public/blog.js
@@ -5,15 +5,7 @@ $(document).ready(() => {
   const year = new Date().getFullYear();
   const url = "https://raw.githubusercontent.com/waterford-montessori-academy/coop-blog/main/posts/" + year + "/" + month + "/" + day + ".json";
   $.getJSON(url, function (data) {
-    const http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    if (http.status != 404) {
-      $(document.body).append("<img style='display: block; margin-left: auto; margin-right: auto; width: 25%; height: 25%;' src='" + data.picture + "'><h1>" + data.title + "</h1>" + "<h3>Today's blog post, courtesy of " + data.author + "</h3><h4>" + data.description + "</h4><h5>" + data.content + "</h5>");
-    }
-    else {
-      $(document.body).append("There is no blog post yet.");
-    }
+    $(document.body).append("<img style='display: block; margin-left: auto; margin-right: auto; width: 25%; height: 25%;' src='" + data.picture + "'><h1>" + data.title + "</h1>" + "<h3>Today's blog post, courtesy of " + data.author + "</h3><h4>" + data.description + "</h4><h5>" + data.content + "</h5>");    
   })
 })
 // @license-end  
