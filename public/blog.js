be850649b68751670048ea8cc37effb57501590e
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2021-10-21 12:36:16 -0400

    Update blog.js

--- public/blog.js
+++ public/blog.js
@@ -3,8 +3,17 @@ $(document).ready(() => {
   const month = new Date().toLocaleString("default", { month: "long" }).toLowerCase();
   const day = new Date().getDate();
   const year = new Date().getFullYear();
-  $.getJSON("https://raw.githubusercontent.com/waterford-montessori-academy/coop-blog/main/posts/" + year + "/" + month + "/" + day + ".json", function (data) {
-    $(document.body).append("<img style='display: block; margin-left: auto; margin-right: auto; width: 25%; height: 25%;' src='" + data.picture + "'><h1>" + data.title + "</h1>" + "<h3>Today's blog post, courtesy of " + data.author + "</h3><h4>" + data.description + "</h4><h5>" + data.content + "</h5>");
+  const url = "https://raw.githubusercontent.com/waterford-montessori-academy/coop-blog/main/posts/" + year + "/" + month + "/" + day + ".json";
+  $.getJSON(url, function (data) {
+    const http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    if (http.status != 404) {
+      $(document.body).append("<img style='display: block; margin-left: auto; margin-right: auto; width: 25%; height: 25%;' src='" + data.picture + "'><h1>" + data.title + "</h1>" + "<h3>Today's blog post, courtesy of " + data.author + "</h3><h4>" + data.description + "</h4><h5>" + data.content + "</h5>");
+    }
+    else {
+      $(document.body).append("There is no blog post yet.");
+    }
   })
 })
 // @license-end  
